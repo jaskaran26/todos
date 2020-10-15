0a19193cce7ed7364fbb1c00322182295178163e
jaskaran123 <jaskaran@multichannelcreative.co.uk>
2020-10-15 13:24:43 +0530

    Updates

---
+++ precache-manifest.52a249992c118aebb97f992478014679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c3738ce3c78b8d18556ad39233f45c",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "68c7fcf5e469d691e1fc",
+    "url": "/todos/static/css/main.13803812.chunk.css"
+  },
+  {
+    "revision": "c5ae2ca8f353b4b8d60c",
+    "url": "/todos/static/js/2.3ee697e7.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todos/static/js/2.3ee697e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c7fcf5e469d691e1fc",
+    "url": "/todos/static/js/main.70159064.chunk.js"
+  },
+  {
+    "revision": "a03f8c18b7a9edf24b3d",
+    "url": "/todos/static/js/runtime-main.e33932e7.js"
+  }
+]);
